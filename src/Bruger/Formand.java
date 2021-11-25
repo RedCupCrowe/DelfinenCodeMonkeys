7fe36d3f0f06d2d983a94398bdf0e0503ab8b155
RedCupCrowe <valdemarkjohansen@gmail.com>
2021-11-25 13:38:37 +0100

    kan nu skrive til fil, sletter dog gamle medlemmer hver gang man gør.

--- src/Bruger/Formand.java
+++ src/Bruger/Formand.java
@@ -10,6 +10,7 @@ public class Formand {
     static Scanner scan = new Scanner(System.in);
 
     public Medlem opretMedlem(){
+
         System.out.println("Navn: " );
         String navn = scan.nextLine();
 
@@ -25,12 +26,10 @@ public class Formand {
         System.out.println("Restance: ");
         boolean restance = scan.nextBoolean();
 
-
-
         Medlem medlem = new Medlem( navn, alder, aktiv,medlemsnummer, restance);
 
         try {
-            FileWriter writer = new FileWriter("medlemsliste.txt");
+            FileWriter writer = new FileWriter("filename.txt");
             writer.write(navn + "\n" + alder + "\n" + aktiv + "\n" + medlemsnummer + "\n" + restance);
             writer.close();
             System.out.println("Successfully wrote to the file.");
@@ -38,6 +37,8 @@ public class Formand {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+
+
         return medlem;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import Medlemmer.MedlemsListe;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,6 +12,17 @@ public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
 
+        try {
+            File myObj = new File("filename.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
         while (true) {
             System.out.println("------------KOMMANDOER------------");
             System.out.println("1. formand      = gå til formandsmenu");
@@ -20,23 +33,19 @@ public class Main {
             System.out.println(command);
 
             switch (command.toLowerCase()) {
-                case "formand", "1":
-                    menu.formandMenu(scan);
+                case "formand", "1" -> menu.formandMenu(scan);
 
 
-                    //formandsmenu
-                    break;
-                case "kasserer", "2":
-                    menu.kassererMenu(scan);
-                    //kasserermenu
-                    break;
-                case "træner", "3": menu.trænerMenu(scan);
-                    //trænermenu
-                    break;
-                default:
+                //formandsmenu
+                case "kasserer", "2" -> menu.kassererMenu(scan);
+
+                //kasserermenu
+                case "træner", "3" -> menu.trænerMenu(scan);
+
+                //trænermenu
+                default ->
                     //Nedenstående skrives ud hvis du ikke skriver en af kommandoerne
                     System.out.println("Det forstår jeg ikke.");
-                    break;
             }
 
         }
